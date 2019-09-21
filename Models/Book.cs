0b84eb133120eb8f4ada2c137342e54e13bc84d0
Vinicius <oliveira.vinicius95@gmail.com>
2019-09-21 13:28:36 -0300

    BookStore

---
+++ Models/Book.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace BookStore.Models
+{
+    public class Book
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public string Genre { get; set; }
+
+        public DateTime DatePublish { get; set; }
+
+        public long Count { get; set; }
+
+          public double Price { get; set; }
+    }
+}
